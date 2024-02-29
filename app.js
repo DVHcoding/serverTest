1a691aa3f01272fad99e2d86663250a1ccb9b3f4
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-01 01:43:52 +0700

    fix some error

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const product = require("./routes/productRoute");
 
 app.use("/api/v1", product);
 
-app.use("/", (req, res) => {
-    res.send(`<h1>${process.env.PORT}</h1>`);
-})
+app.get("/", (req, res) => {
+    const PORT = process.env.PORT || 3000; // Use port 3000 as default if PORT is not defined
+    res.send(`<h1>Server is running on port ${PORT}</h1>`);
+});
 
 module.exports = app;
--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDatabase = () => {
-    mongoose.connect("mongodb+srv://dvhcoding:0359123821@ecommerce.ojkcioz.mongodb.net/?retryWrites=true&w=majority&appName=Ecommercev")
+    mongoose.connect(process.env.DB_URI)
         .then((data) => {
             console.log(`Mongodb connect with server: ${data.connection.host}`);
         })
