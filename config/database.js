2bfba659d0d76764430ddaef63a142f5f4b82bbc
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-01 02:32:57 +0700

    fix railway

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI)
+    mongoose.connect(process.env.MONGO_PRIVATE_URL)
         .then((data) => {
             console.log(`Mongodb connect with server: ${data.connection.host}`);
         })
