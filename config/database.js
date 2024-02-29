951b4d6d3405bd68cb1ac2cd0a255040d5a9104a
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-01 01:32:49 +0700

    fix mongoose

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI)
+    mongoose.connect("mongodb+srv://dvhcoding:0359123821@ecommerce.ojkcioz.mongodb.net/?retryWrites=true&w=majority&appName=Ecommercev")
         .then((data) => {
             console.log(`Mongodb connect with server: ${data.connection.host}`);
         })
