fc8568d3e2211f0930155faf16cacf51e79010d1
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-01 02:34:56 +0700

    fix mongourl

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_PRIVATE_URL)
+    mongoose.connect(process.env.MONGO_URL)
         .then((data) => {
             console.log(`Mongodb connect with server: ${data.connection.host}`);
         })
